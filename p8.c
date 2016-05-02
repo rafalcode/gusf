765dba3edd50d16ad915111775085bd94e7c1a93
Ramon Fallon <ramonfallon@gmail.com>
2016-05-02 12:22:09 +0100

    fir

---
+++ p8.c
@@ -0,0 +1,13 @@
+/* The first of a series of Dan Gusfield code snippets */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+   char *s="aabaabcaxaabaabcy";
+
+   printf("Pstring length is: %zu\n", strlen(s));
+
+   return 0;
+}
