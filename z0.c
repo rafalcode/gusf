47f140af70e3e294179f0bb454c3efb7b3cdfb52
Ramon Fallon <ramonfallon@gmail.com>
2017-03-27 10:12:30 +0100

    latest

--- z0.c
+++ z0.c
@@ -1,33 +1,35 @@
-/* The first of a series of Dan Gusfield code snippets */
+/* Calculates the size of the zbox forThe first of a series of Dan Gusfield code snippets */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[])
 {
-    int i;
-   char *s="aabaabcaxaabaabcy";
+	int i;
+	char *s="aabaabcaxaabaabcy";
 
-   unsigned slen=strlen(s);
-   unsigned *zl=calloc(slen, sizeof(unsigned));
-   char *sp1, *sp2; // two ptrs to travel along s.
+	unsigned slen=strlen(s);
+	unsigned *zl=calloc(slen, sizeof(unsigned));
+	char *sp1, *sp2; // two ptrs to travel along s.
 
-   for(i=0;i<slen;++i) {
-       sp1=s+i;
-       sp2=s;
-       while(*sp1) {
-           if(*sp1++ == *sp2++)
-                zl[i]++;
-           else
-               break;
-       }
-   }
+	for(i=0;i<slen;++i) {
+		sp1=s+i;
+		sp2=s;
+		while(*sp1) {
+			if(*sp1++ == *sp2++)
+				zl[i]++;
+			else
+				break;
+		}
+	}
 
-    for(i=0;i<slen;++i) 
-        printf("%u ", zl[i]);
-    printf("\n"); 
+	printf("The target string was \"%s\"\n", s); 
+	for(i=0;i<slen;++i) 
+		printf("%u ", zl[i]);
+	printf("\n"); 
+	printf("Note first value is trivial, really should be left out\n"); 
 
-    free(zl);
+	free(zl);
 
-   return 0;
+	return 0;
 }
